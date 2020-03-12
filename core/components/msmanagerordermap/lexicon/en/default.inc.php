2387ef49c8d53069e73790972827d79d4cf3795c
webinmd <89poilo@gmail.com>
2020-03-12 22:03:33 +0200

    Fix lexicon

--- core/components/msmanagerordermap/lexicon/en/default.inc.php
+++ core/components/msmanagerordermap/lexicon/en/default.inc.php
@@ -1,7 +1,7 @@
 <?php 
 
-$_lang['msmanagerorderaddressmap'] = 'msManagerOrderAddressMap';
+$_lang['msmanagerordermap'] = 'msManagerOrderMap';
 
-$_lang['error_msmanageraddressmap_key_empty'] = 'msManagerOrderAddressMap: specify in system settings yandex api key. You can get the key here: <a href="https://developer.tech.yandex.ru/services/" target="_blank">https://developer.tech.yandex.ru/services/</a>';
+$_lang['error_msmanagerordermap_key_empty'] = 'msManagerOrderMap: specify in system settings yandex api key. You can get the key here: <a href="https://developer.tech.yandex.ru/services/" target="_blank">https://developer.tech.yandex.ru/services/</a>';
 
-$_lang['error_msmanageraddressmap_address_fields_empty'] = 'msManagerOrderAddressMap: specify in the system settings at least one field to get the address';
+$_lang['error_msmanagerordermap_address_fields_empty'] = 'msManagerOrderMap: specify in the system settings at least one field to get the address';
